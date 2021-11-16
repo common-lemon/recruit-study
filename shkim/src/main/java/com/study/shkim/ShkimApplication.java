b6822f5409d479e0e3be6b12fc6657e5c3687381
shkim <asaash133@gmail.com>
2021-11-16 16:14:00 +0900

    init

---
+++ shkim/src/main/java/com/study/shkim/ShkimApplication.java
@@ -0,0 +1,13 @@
+package com.study.shkim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShkimApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShkimApplication.class, args);
+    }
+
+}
---
+++ shkim/src/test/java/com/study/shkim/ShkimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.shkim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShkimApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
