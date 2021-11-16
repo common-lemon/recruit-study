9e316f1cd71d57c67c31409762ea11384cdb2a7a
Jaehyeok PC <zcad8546@naver.com>
2021-11-16 16:13:26 +0900

    2021-11-16 INIT 이재혁

---
+++ jaehyeok/src/main/java/com/study/jaehyeok/JaehyeokApplication.java
@@ -0,0 +1,13 @@
+package com.study.jaehyeok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaehyeokApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JaehyeokApplication.class, args);
+    }
+
+}
---
+++ jaehyeok/src/test/java/com/study/jaehyeok/JaehyeokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.jaehyeok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaehyeokApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
