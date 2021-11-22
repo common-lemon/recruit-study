062782d94bcbaeff2d8e8ee38e27281aeca9b62f
Hyunseong <purmir2@gmail.com>
2021-11-22 18:08:31 +0900

    2021.11.22.(월) hyunseong

---
+++ hyunseong/src/main/java/com/study/book/rest/model/BookEntity.java
@@ -0,0 +1,32 @@
+package com.study.book.rest.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "books")
+public class BookEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(length = 30)
+    private String bookNo;
+    @Column(length = 30)
+    private String deptName;
+    @Column(length = 255)
+    private String title;
+    @Column(length = 30)
+    private String registerNm;
+    @Column(length = 50)
+    private String publisher;
+    private int bookPrice;
+    @Column(length = 255)
+    private String regRsn;
+    private int count;
+
+}
---
+++ hyunseong/src/main/java/com/study/book/rest/svr/itf/BookSvrItf.java
@@ -0,0 +1,13 @@
+package com.study.book.rest.svr.itf;
+
+import com.study.book.rest.dto.ReqBookParams;
+import com.study.book.rest.dto.ResBook;
+
+public interface BookSvrItf {
+
+    ResBook list(ReqBookParams reqBookParams);
+    ResBook save(ReqBookParams reqBookParams);
+    ResBook delete(ReqBookParams reqBookParams);
+    ResBook detail(ReqBookParams reqBookParams);
+
+}
