110a44b96bb59ed7eef534cc0f46d37840cb9f03
SH Kim <82145134+shkim-123@users.noreply.github.com>
2021-11-24 00:20:48 +0900

    211123 html

--- shkim/src/main/java/com/study/shkim/rest/ctl/BookCtl.java
+++ shkim/src/main/java/com/study/shkim/rest/ctl/BookCtl.java
@@ -7,9 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -30,7 +28,7 @@ public class BookCtl {
     }
 
     @RequestMapping(value = "/save", method = RequestMethod.POST)
-    public ResponseEntity<ResBook> save(ReqBookParams params){
+    public ResponseEntity<ResBook> save(@RequestBody ReqBookParams params){
         log.info("[정보] params : {}", params.toString());
         return new ResponseEntity<>(this.bookSvr.save(params), HttpStatus.OK);
     }
