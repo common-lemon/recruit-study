a049ff7480b55b9d26407e03cea67087f85490c4
shkim <asaash133@gmail.com>
2021-11-26 17:58:28 +0900

    211126 spring security

--- shkim/src/main/java/com/study/shkim/security/account/AccountService.java
+++ shkim/src/main/java/com/study/shkim/security/account/AccountService.java
@@ -1,6 +1,7 @@
 package com.study.shkim.security.account;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -8,6 +9,9 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Collection;
+
+@Slf4j
 @Service
 @AllArgsConstructor
 public class AccountService implements UserDetailsService {
@@ -23,6 +27,9 @@ public class AccountService implements UserDetailsService {
             throw new UsernameNotFoundException(username);
         }
 
+        log.info("[정보] account : {}", account.getDeptName());
+        log.info("[정보] account : {}", account.getRegisterNm());
+
         return User.builder()
                 .username(account.getUsername())
                 .password(account.getPassword())
@@ -34,4 +41,5 @@ public class AccountService implements UserDetailsService {
         account.encodePassword(passwordEncoder);
         return accountRepository.save(account);
     }
+
 }
