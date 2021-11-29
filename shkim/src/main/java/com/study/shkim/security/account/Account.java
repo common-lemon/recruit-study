0fbc58c2bfc5a589dc23298fe854b70cbb1ec0c3
shkim <asaash133@gmail.com>
2021-11-29 18:06:01 +0900

    211129 spring security, html 수정

--- shkim/src/main/java/com/study/shkim/security/account/Account.java
+++ shkim/src/main/java/com/study/shkim/security/account/Account.java
@@ -16,16 +16,13 @@ public class Account {
 
     @Id @GeneratedValue
     private Integer id;
-
     @Column(unique = true)
     private String username;
-
     private String password;
-
     private String role;
-
+    @Column(length = 30)
     private String deptName;
-
+    @Column(length = 30)
     private String registerNm;
 
     public void encodePassword(PasswordEncoder passwordEncoder) {
